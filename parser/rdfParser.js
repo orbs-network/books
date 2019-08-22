69e70a1e7a49cd05a11861887d71e62dffa8ee31
gilda <gil@daganmail.com>
2019-08-22 11:50:50 +0300

    code formatting

--- parser/rdfParser.js
+++ parser/rdfParser.js
@@ -1,82 +1,83 @@
 // this program takes rdf data from the gutenberg project about their collection
 // and converts it to JSON data containing the interesting parts of the data.
 
-const fs = require("fs")
-const xmlParser = require("xml2json")
+const fs = require("fs");
+const xmlParser = require("xml2json");
 
 // returns the json representation of an rdf file from the gutenberg project
 // if the file does not exist it returns null
-function toJson(rdf){
-	// get rid of rdf bloat
-	rdf = rdf.replace(new RegExp("dcterms:", "g"), "")
-	rdf = rdf.replace(new RegExp("pgterms:", "g"), "")
-	rdf = rdf.replace(new RegExp("rdf:", "g"), "")
-	
-	// turn rdf xml to JSON
-	convertedJson = xmlParser.toJson(rdf)
+function toJson(rdf) {
+  // get rid of rdf bloat
+  rdf = rdf.replace(new RegExp("dcterms:", "g"), "");
+  rdf = rdf.replace(new RegExp("pgterms:", "g"), "");
+  rdf = rdf.replace(new RegExp("rdf:", "g"), "");
 
-	// create the JSON object and get rid of more rdf bloat
-	data = JSON.parse(convertedJson)
-	data = data["RDF"]["ebook"]
+  // turn rdf xml to JSON
+  convertedJson = xmlParser.toJson(rdf);
 
-	// construct the main book object from the JSON data
-	book = {
-		Author: data.creator.agent.name,
-		FileFormats: data["hasFormat"],
-		Issued: data["issued"]["$t"],
-		Language: data["language"].Description.value["$t"],
-		Link: "http://www.gutenberg.org/" + data["about"],
-		Publisher: data["publisher"],
-		Rights: data["rights"],
-		Subjects: data["subject"],
-		Title: data["title"],
-	}
+  // create the JSON object and get rid of more rdf bloat
+  data = JSON.parse(convertedJson);
+  data = data["RDF"]["ebook"];
 
-	// remove file format rdf and xml bloat
-	for(i = 0; i < book.FileFormats.length; i++){
-		// removing bloat
-		book.FileFormats[i] = {
-			Link: book.FileFormats[i].file.about,
-			Format: book.FileFormats[i].file.format
-		}
-		
-		// removing more bloat
-		if(book.FileFormats[i].Format.length == undefined){
-			book.FileFormats[i].Format = [book.FileFormats[i].Format.Description.value["$t"]]
-			continue
-		}
+  // construct the main book object from the JSON data
+  book = {
+    Author: data.creator.agent.name,
+    FileFormats: data["hasFormat"],
+    Issued: data["issued"]["$t"],
+    Language: data["language"].Description.value["$t"],
+    Link: "http://www.gutenberg.org/" + data["about"],
+    Publisher: data["publisher"],
+    Rights: data["rights"],
+    Subjects: data["subject"],
+    Title: data["title"]
+  };
 
-		// iterativley removing bloat
-		for(j = 0; j < book.FileFormats[i].Format.length; j++){
-			book.FileFormats[i].Format[j] = book.FileFormats[i].Format[j].Description.value["$t"]
-		}
-	}
+  // remove file format rdf and xml bloat
+  for (i = 0; i < book.FileFormats.length; i++) {
+    // removing bloat
+    book.FileFormats[i] = {
+      Link: book.FileFormats[i].file.about,
+      Format: book.FileFormats[i].file.format
+    };
 
-	// more bloat removal
-	if(book.Subjects.length == undefined){
-		book.Subjects = [book.Subjects.Description.value["$t"]]
-	}
+    // removing more bloat
+    if (book.FileFormats[i].Format.length == undefined) {
+      book.FileFormats[i].Format = [
+        book.FileFormats[i].Format.Description.value["$t"]
+      ];
+      continue;
+    }
 
-	for(i = 0; i < book.Subjects.length; i++){
-		book.Subjects[i] = book.Subjects[i].Description.value
-	}
+    // iterativley removing bloat
+    for (j = 0; j < book.FileFormats[i].Format.length; j++) {
+      book.FileFormats[i].Format[j] =
+        book.FileFormats[i].Format[j].Description.value["$t"];
+    }
+  }
 
-	// return final book JSON representation
-	return JSON.stringify(book)
+  // more bloat removal
+  if (book.Subjects.length == undefined) {
+    book.Subjects = [book.Subjects.Description.value["$t"]];
+  }
 
+  for (i = 0; i < book.Subjects.length; i++) {
+    book.Subjects[i] = book.Subjects[i].Description.value;
+  }
+
+  // return final book JSON representation
+  return JSON.stringify(book);
 }
 
-function fileToJson(fileName){
-	return toJson(getRDFfile(fileName))
+function fileToJson(fileName) {
+  return toJson(getRDFfile(fileName));
 }
 
-function getRDFfile(fileName){
-	return fs.readFileSync(fileName).toString()
+function getRDFfile(fileName) {
+  return fs.readFileSync(fileName).toString();
 }
 
 // export the function for the library
 module.exports = {
-	toJson,
-	fileToJson
-}
-
+  toJson,
+  fileToJson
+};
--- src/bookIndex.js
+++ src/bookIndex.js
@@ -1,89 +1,89 @@
-const { argString, argUint64 } = require("orbs-client-sdk")
+const { argString, argUint64 } = require("orbs-client-sdk");
 
 class BookIndex {
-    constructor(account, client){
-        this.name = `bookIndex.${new Date().getTime()}`
-        this.account = account
-        this.client = client
+  constructor(account, client) {
+    this.name = `bookIndex.${new Date().getTime()}`;
+    this.account = account;
+    this.client = client;
+  }
+
+  async registerBooks(books) {
+    const [tx, txId] = this.client.createTransaction(
+      this.account.publicKey,
+      this.account.privateKey,
+      this.name,
+      "registerBooks",
+      [argString(JSON.stringify(books))]
+    );
+
+    const result = await this.client.sendTransaction(tx);
+
+    if (result.executionResult != "SUCCESS") {
+      return new Error("transaction did not succeed!");
     }
 
-    async registerBooks(books){
-        const [tx, txId] = this.client.createTransaction(
-            this.account.publicKey,
-            this.account.privateKey,
-            this.name,
-            "registerBooks",
-            [argString(JSON.stringify(books))]
-		)
-		
-		const result = await this.client.sendTransaction(tx)
-		
-		if(result.executionResult != "SUCCESS"){
-			return new Error("transaction did not succeed!")
-		}
-		
-		return JSON.parse(result.outputArguments[0].value)
+    return JSON.parse(result.outputArguments[0].value);
+  }
+
+  async getBooks(start, limit) {
+    const [tx, txId] = this.client.createTransaction(
+      this.account.publicKey,
+      this.account.privateKey,
+      this.name,
+      "getBooks",
+      [argUint64(start), argUint64(limit)]
+    );
+
+    const result = await this.client.sendTransaction(tx);
+
+    if (result.executionResult != "SUCCESS") {
+      return new Error("transaction did not succeed!");
     }
 
-    async getBooks(start, limit){
-        const [tx, txId] = this.client.createTransaction(
-            this.account.publicKey,
-            this.account.privateKey,
-            this.name,
-            "getBooks",
-            [argUint64(start), argUint64(limit)]
-		)
-		
-		const result = await this.client.sendTransaction(tx)
-
-		if(result.executionResult != "SUCCESS"){
-			return new Error("transaction did not succeed!")
-		}
-		
-		if(result.outputArguments[0].value == ""){
-			return null
-		}
-
-		return JSON.parse(result.outputArguments[0].value)
-	}
-
-	async getBook(id){
-		const [tx, txId] = this.client.createTransaction(
-			this.account.publicKey,
-			this.account.privateKey,
-			this.name,
-			"getBook",
-			[argUint64(id)]
-		)
-
-		const result = await this.client.sendTransaction(tx)
-
-		if(result.executionResult != "SUCCESS"){
-			return new Error("transaction did not succeed!")
-		}
-
-		return JSON.parse(result.outputArguments[0].value)
-	}
-
-    async totalBooks(){
-        const [tx, txId] = this.client.createTransaction(
-            this.account.publicKey,
-            this.account.privateKey,
-            this.name,
-            "totalBooks",
-            []
-		)
-		
-		const result = await this.client.sendTransaction(tx)
-		
-		if(result.executionResult != "SUCCESS"){
-			return new Error("transaction did not succeed!")
-		}
-		
-		return Number(result.outputArguments[0].value)
+    if (result.outputArguments[0].value == "") {
+      return null;
     }
+
+    return JSON.parse(result.outputArguments[0].value);
+  }
+
+  async getBook(id) {
+    const [tx, txId] = this.client.createTransaction(
+      this.account.publicKey,
+      this.account.privateKey,
+      this.name,
+      "getBook",
+      [argUint64(id)]
+    );
+
+    const result = await this.client.sendTransaction(tx);
+
+    if (result.executionResult != "SUCCESS") {
+      return new Error("transaction did not succeed!");
+    }
+
+    return JSON.parse(result.outputArguments[0].value);
+  }
+
+  async totalBooks() {
+    const [tx, txId] = this.client.createTransaction(
+      this.account.publicKey,
+      this.account.privateKey,
+      this.name,
+      "totalBooks",
+      []
+    );
+
+    const result = await this.client.sendTransaction(tx);
+
+    if (result.executionResult != "SUCCESS") {
+      return new Error("transaction did not succeed!");
+    }
+
+    return Number(result.outputArguments[0].value);
+  }
 }
 
 module.exports = {
-    BookIndex
-}
+  BookIndex
+};
