2598c9e354ca8e4a79beda5fce6475c4d48fe95d
gilda <gil@daganmail.com>
2019-09-12 15:35:23 +0300

    add webui to interact with the contract

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 const http = require("http");
 const fs = require("fs")
 const url = require("url")
-const uploader = require("./uploader")
 const dynamo = require("./dynamo")
 
 http.createServer(async (req, res) => {
@@ -9,6 +8,17 @@ http.createServer(async (req, res) => {
 	if(req.url == "/"){
 		res.writeHead(200, {'Content-Type': 'text/html'});
 		res.write(fs.readFileSync("./src/webui/index.html").toString())
+		res.end()
+	}else if(req.url == "/background.png"){
+		res.writeHead(200, {'Content-Type': 'image/png'});
+		res.end(fs.readFileSync("./background.png"), 'binary')
+	}else if(req.url == "/orbs-client-sdk-web.js"){
+		res.writeHead(200, {'Content-Type': 'text/javascript'})
+		res.write(fs.readFileSync("./node_modules/orbs-client-sdk/dist/orbs-client-sdk-web.js"))
+		res.end()
+	}else if(req.url == "/webClient.js"){
+		res.writeHead(200, {'Content-Type': 'text/javascript'})
+		res.write(fs.readFileSync("./src/webClient.js"))
 		res.end()
 	}else if(webUrl.pathname == "/search"){
 		res.writeHead(200, {'Content-Type': 'text/html'});
@@ -21,10 +31,11 @@ http.createServer(async (req, res) => {
 			res.write("Error, query is invalid")
 			res.end()
 		}else{
-			dynamo.searchBook(getParams(webUrl.query), writeToRes, res)
+			dynamo.searchBook(getParams(webUrl.query), writeTable, res, getParams(webUrl.query))
 		}
+	}else{
+		res.end()
 	}
-
 }).listen(5050)
 
 function getParams(query){
@@ -40,9 +51,56 @@ function getParams(query){
 	return ret
 }
 
-// TODO stylize the search results
-function writeToRes(data, args){
+function writeTable(data, args){
 	res = args[0]
-	res.write(JSON.stringify(data))
+	params = args[1]
+	ret = `
+		<html>
+			<head>
+				<title>Orbs Book Registry</title>
+				<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>
+			</head>
+			<body style='text-align: center; background-image: url(\"background.png\"); background-color: rgb(0,0,50)'>
+				<div style='color: white'>
+					<h1 style='text-align: center'>Orbs Book Registry Project</h1>
+					<div style='height: 30%'>
+						<h3 style='height: 20%'>Books that match Title: '${params.title}' and Author: '${params.Author}'</h3>
+						<div style='height: 60%'>
+							<button style='vertical-align: middle' class='btn btn-primary' onclick='window.location.href=\"/\"'>Search again</button>
+						</div>
+					</div>
+					<table style='text-align: center; color: white' id='result' class='table table-dark table-hover table-bordered'>
+						<thead class='thead-dark'>
+							<th>Orbs Rgistry Book ID</th>
+							<th>Author</th>
+							<th>Title</th>
+							<th>Publication Date</th>
+							<th>Publisher</th>
+						</thead>
+	`
+						for(i = 0; i < data.Items.length; i++){
+							ret += "<tr onclick='displayBook(" + data.Items[i].Id + ")'><td>" + data.Items[i].Id + "</td>"
+							ret += "<td>" + data.Items[i].Author + "</td>"
+							ret += "<td>" + data.Items[i].Title + "</td>"
+							ret += "<td>" + data.Items[i].Issued + "</td>"
+							ret += "<td>" + data.Items[i].Publisher + "</td></tr>"
+						}
+
+	ret += `
+					</table>
+					<p style='text-align: center; height: 20%'>Found ${data.Count} items out of ${data.ScannedCount}</p>
+				</div>
+			</body>
+			<script src='orbs-client-sdk-web.js'></script>
+			<script src='webClient.js'></script>
+			<script>
+				window.onload = () => {
+					init()
+				}	
+			</script>
+		</html>
+	`
+	
+	res.write(ret)
 	res.end()
 }
---
+++ src/webClient.js
@@ -0,0 +1,87 @@
+var client = {}
+var account = {}
+ORBS_NODE_ADDRESS = undefined
+ORBS_VCHAIN = undefined
+
+function init(){
+	const endpoint = ORBS_NODE_ADDRESS || "http://localhost:8080";
+	const chain = Number(ORBS_VCHAIN) || 42;
+	client = new Orbs.Client(
+		endpoint,
+		chain,
+		Orbs.NetworkType.NETWORK_TYPE_TEST_NET
+	);
+	account = Orbs.createAccount()
+}
+
+async function getBook(id){
+	const [tx, _] = client.createTransaction(
+		account.publicKey,
+		account.privateKey,
+		"Gilda",
+		"getBooks",
+		[Orbs.argUint64(id), Orbs.argUint64(1)]
+	);
+
+	const result = await client.sendTransaction(tx);
+
+	if (result.executionResult != "SUCCESS") {
+		if (result.outputArguments[0].value == "no such book id") {
+			return new Error("no such book id");
+		}
+		return new Error(result.outputArguments[0].value);
+	}
+
+	if (result.outputArguments[0].value == "") {
+		return null;
+	}
+
+	return JSON.parse(result.outputArguments[0].value)[0];
+}
+
+async function displayBook(bookId){
+	book = await getBook(bookId)
+	console.log(book)
+
+	// YES YES POSSIBLE XSS attack if someone publishes to smart contract <scriptbook>
+	ret = `
+		<div style="color: white">
+			<h1 style="text-align: center">Orbs Book Registry Project</h1>
+			<br>
+			<h4 style="text-align: center">This is the Orbs Book Registry project.<br>Its main goal is to register and verify Public Domain Books on the Orbs blockchain.<br>You can search for books on this page.</h4>
+			<br>
+			<br>
+			<p>Title: ${book.Title}</p>
+			<p>Author: ${book.Author}</p>
+			<p>Publication Date: ${book.Issued}</p>
+			`
+			for(i = 0; i < book.Publishers.length; i++){
+				ret += `
+				<table class='table table-bordered'>
+					<tr>
+						<p>Publisher: ${book.Publishers[i].Name}</p>
+						<a href='${book.Publishers[i].MetadataLink}'>Link to book on Publisher's website</p>`
+						for(j = 0; j < book.Publishers[i].FileVersions.length; j++){
+							ret += `
+								<a href='${book.Publishers[i].FileVersions[j].Link}'>book in ${book.Publishers[i].FileVersions[j].Format.join(" ")} format</a>
+								<br>
+							`
+						}
+					ret += `	
+					</tr>
+				</table>
+				<br>
+				`
+			}
+	ret +=  `
+			<p>Regisrty ID: ${bookId}</p>
+		</div>
+		<style>
+			a {
+				color: white;
+			}
+		</style>
+	`
+
+	document.body.innerHTML = ret
+}
